011ea9ff44ebc270b4056a3cbee58e5b43a97a17
bklimekkk <klimekbartosz12@gmail.com>
2022-02-07 12:52:03 +0000

    Initial Commit

---
+++ Share Your Journey/Share_Your_JourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Share_Your_JourneyApp.swift
+//  Share Your Journey
+//
+//  Created by Bartosz Klimek on 07/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Share_Your_JourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
